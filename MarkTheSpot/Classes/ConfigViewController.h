cfaedca6c7d38e9ee80ee3eeae084ed032c22e64
Jon Pedersen <jon@schantz.com>
2013-02-25 09:44:02 +0100

    initial

---
+++ MarkTheSpot/Classes/ConfigViewController.h
@@ -0,0 +1,25 @@
+//
+//  ConfigViewController.h
+//  MarkTheSpot
+//
+//  Created by Jon Pedersen on 07/11/08.
+//  Copyright 2008 __MyCompanyName__. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "MarkTheSpotAppDelegate.h"
+
+
+@interface ConfigViewController : UIViewController {
+	IBOutlet UISwitch *usSwitch;
+	IBOutlet UIButton *doneButton;
+	MarkTheSpotAppDelegate *delegate;
+}
+
+@property (nonatomic, retain) UISwitch *usSwitch;
+@property (nonatomic, retain) UIButton *doneButton;
+@property (nonatomic, retain) MarkTheSpotAppDelegate *delegate;
+
+- (void)doneButtonPressed:(id)sender;
+
+@end
